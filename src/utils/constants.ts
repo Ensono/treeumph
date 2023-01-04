2103908b48f96291da27ec2dc7d9c64cc5085970
Matthew Aizlewood <aizlewood40@live.co.uk>
2023-01-04 11:22:05 +0000

    chore: update bob id

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,9 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const COMPANY_NAME = "Ensono Digital";
-// This is currently set to the user id for the "CW Treeumph" bot in slack
-// @TODO: Update this to be the user id for the BOB HR Bot
-export const HR_BOT_USER_ID = "U04FFG30QUV";
+export const HR_BOT_USER_ID = "UNGNQ7X16";
 export const MONTHLY_TREE_BUDGET = 30;
 export const S3_BUCKET_NAME = "ensono-treeumph-bucket-store";
 export const TREE_EMOJI_NAME = "deciduous_tree";
